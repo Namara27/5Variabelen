73e8f5a24fa2bfe9d2f88cc0b3d37e1efd281e8d
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-30 15:57:10 +0200

    tweede commit

--- src/Opdracht5_2/StaafdiagramNieuw.java
+++ src/Opdracht5_2/StaafdiagramNieuw.java
@@ -1,4 +1,14 @@
 package Opdracht5_2;
 
+import java.awt.*;
+import java.applet.*;
+
 public class StaafdiagramNieuw {
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+            
+        }
 }
